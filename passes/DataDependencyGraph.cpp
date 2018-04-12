e0328ec2028764e8819f6b9e904d9b5aec1079bb
Chirag Bharadwaj <cb625@cornell.edu>
2018-04-12 03:21:01 -0400

    Wrote stubs for DDG implementation

--- passes/DataDependencyGraph.cpp
+++ passes/DataDependencyGraph.cpp
@@ -7,4 +7,28 @@ using namespace llvm;
 // Shared namespace within the project.
 namespace apollo {
 
+  // See header file for comments.
+
+  char DataDependencyPass::ID = 0; // Default value.
+
+  bool DataDependencyPass::runOnFunction(Function &F) {
+    return false;
+  }
+
+  StringRef DataDependencyPass::getPassName() const {
+    return "";
+  }
+
+  void DataDependencyPass::releaseMemory() {
+    return;
+  }
+
+  void DataDependencyPass::getAnalysisUsage(AnalysisUsage &info) const {
+    return;
+  }
+
+  const Graph<Instruction> &DataDependencyPass::getGraph() const {
+    return ddeps;
+  }
+
 }
