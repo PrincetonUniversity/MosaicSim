9642cb45a8c4df3aeb821a1dd91eedf3131c0faf
juanlaragon <jlaragon@cafe.CS.Princeton.EDU>
2018-05-09 17:02:09 -0400

    initial files for simulator

---
+++ sim/sim-apollo.cpp
@@ -0,0 +1,24 @@
+//=======================================================================
+// Copyright 2018 University of Princeton.
+//
+// Project Apollo - simulator
+// Authors: 
+//=======================================================================// Project Apollo - simulator
+
+#include "sim-apollo.hpp"
+using namespace apollo;
+
+#include <iostream>                         
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+   /* code */
+   Graph g;
+
+   //g.insertNode(
+   
+   cout << "hello";
+   return 0;
+}
---
+++ sim/sim-apollo.hpp
@@ -0,0 +1,94 @@
+//=======================================================================
+// Copyright 2018 University of Princeton.
+//
+// Project Apollo - simulator
+// Authors: 
+//=======================================================================// Project Apollo - simulator
+
+// Header file
+
+#include <set>
+#include <vector>
+#include <iostream>                         
+#include <algorithm>
+
+namespace apollo {
+
+typedef enum {add, sub, mult, div, ld, st, branch_cond, branch_uncond} TInstr;
+typedef enum {true_mem_dep, maybe_mem_dep, CF_dep} TEdge;
+
+class Node;
+
+class Edge {
+   private:
+      TEdge edge_type;
+      Node *src;
+      Node *dst;
+
+   public:
+      Edge(Node *s, Node *d, TEdge type) : src(s), dst(d), edge_type(type) {}
+};
+
+
+class Node {
+   private:
+      int instr_id;
+      std::string instr_name;
+      TInstr instr_type;
+      std::set<Edge *> adjs;
+   
+   public:
+      Node(TInstr type, int id) : instr_type(type), instr_id(id) {}
+   
+      void insertEdge(Edge *e) {
+         adjs.insert(e);  
+      }
+   
+      void eraseEdge(Edge *e) {
+         adjs.erase( e );
+      }
+   
+      void insertEdge(Node *dest, TEdge type) {
+         adjs.insert( new Edge(this, dest, type) );  
+      }
+   
+      void eraseEdge(Node *dest) {
+         Edge *e = adjs.find();
+         eraseEdge(e);
+         delete e;
+      }
+   
+      friend std::ostream &operator<<(std::ostream &os, const Node &n) {
+         os << n.instr_name << ": " << n.adjs << "\n";
+      }
+};
+
+
+class Graph {
+   private:
+      std::set<Node *> nodes;
+
+   public:
+      Graph() {}
+   
+      int get_num_nodes() { return nodes.size(); }
+   
+      void insertNode(TInstr instr_type, int instr_id, std::string instr_name = "") {
+         nodes.insert( new Node(instr_type, instr_id) );
+      }
+   
+      void eraseNode(Node *n) { 
+         nodes.erase( n ); 
+         delete n;
+      }
+   
+      void insertEdge(Node *s, Node *d, TEdge type) {
+         s->insertEdge(d, type);
+      }
+
+      void eraseEdge(Node *s, Node *d) {
+         s->eraseEdge(d);
+      }
+};
+
+}
