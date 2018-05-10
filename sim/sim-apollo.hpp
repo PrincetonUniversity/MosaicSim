44a4aae9040a2669d30ad84a2aee173940650324
juanlaragon <jlaragon@cafe.CS.Princeton.EDU>
2018-05-09 21:45:31 -0400

    msg

--- sim/sim-apollo.hpp
+++ sim/sim-apollo.hpp
@@ -56,17 +56,16 @@ class Node {
       }
    
       void eraseEdge(Node *dest) {
-         Edge *e; // = adjs.find();
+         Edge *e; //= adjs.find();
          eraseEdge(e);
          delete e;
       }
    
       friend std::ostream &operator<<(std::ostream &os, const Node &n) {
-         os << n.instr_name << ": ";
+         os << n.instr_name << ": Adj = [";
          for ( std::set<Edge *>::iterator it = n.adjs.begin(); it != n.adjs.end(); ++it )
-            std::cout << ", " << it->dst.instr_name;
-
-         cout << endl;
+            std::cout << *it->dst->instr_name << ", " ;
+         cout << "]" << endl;
       }
 };
 
