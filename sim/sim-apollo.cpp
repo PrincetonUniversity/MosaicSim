d1e7ed3b29e72b24b43c8b12a8f735532bcb026e
Tae Jun <ham.taejun@gmail.com>
2018-06-13 02:05:26 -0400

    cleanup

--- sim/sim-apollo.cpp
+++ sim/sim-apollo.cpp
@@ -1,49 +1,20 @@
-//=======================================================================
-//
-// Project Apollo - simulator
-// Authors: 
-//=======================================================================
-
 #include "sim-apollo.hpp"
 using namespace apollo;
-
-#include <iostream> 
-#include <map>
-#include "assert.h"
-#include "dramsim2/DRAMSim.h"
-#include <vector>
-#include <cstdio> 
 using namespace std;
 
-class Context;
+#include "dramsim2/DRAMSim.h"
+#include <iostream> 
+#include <cstdio> 
 
+Graph g;
 int cycle_count = 0;
-int recent_context_id = 0;
-std::map<uint64_t, std::pair<Node*,Context*> > outstanding_access_map; 
-std::map<int, BasicBlock*> bb_map; // FIX
+int curr_context_id = 0;
 std::vector<Context*> context_list;
-
+std::map<uint64_t, std::pair<Node*,Context*> > outstanding_access_map; 
 DRAMSim::MultiChannelMemorySystem *mem;
 
 // TODO: Memory address overlap across contexts
-class Context
-{
-public:
-   Context(int id, int bid) {
-      live = true;
-      cid = id;
-      bbid = bid;
-      count = 0;
-   }
-   bool live;
-   int cid;
-   int bbid; // bb id that this context represents
-   int count;
-   std::vector<Node*> active_list;
-   std::set<Node*> newly_active_nodes;
-   std::map<Node*, int> latency_map;
-   std::map<Node*, int> dep_map;
-};
+
 class DRAMSimCallBack
 {
 public: 
@@ -63,11 +34,6 @@ public:
       outstanding_access_map.erase(address);
    }
 };
-int peek_next_bb() 
-{
-   // return next bb id
-   return -1;
-}
 
 void process_context(Context *c)
 {
@@ -76,32 +42,33 @@ void process_context(Context *c)
    for ( int i=0; i < c->active_list.size(); i++ ) {
       Node *n = c->active_list.at(i);
       if(c->newly_active_nodes.find(n) != c->newly_active_nodes.end()) {
-         std::cout << "Node [" << n->instr_name << "] starts execution\n";
-         if(n->instr_type == LD) {
-            // willAcceptTransaction
-            uint64_t addr = n->instr_id * 64; // Todo: FAKE ADDRESS
+         std::cout << "Node [" << n->name << "] starts execution\n";
+         if(n->type == LD) {
+            // TODO: WillAcceptTransaction
+            uint64_t addr = n->id * 64; // TODO: Use Real Address
             mem->addTransaction(false, addr);
-            std::cout << "Inserted a Read Transaction from Node " << n->instr_name << "\n";
+            std::cout << "Inserted a Read Transaction from Node " << n->name << "\n";
             outstanding_access_map.insert(std::make_pair(addr, std::make_pair(n,c)));
          }
-         else if(n->instr_type == ST) {
-            uint64_t addr = n->instr_id * 64; // Todo: FAKE ADDRESS
+         else if(n->type == ST) {
+            uint64_t addr = n->id * 64; // TODO: Use Real Address
             mem->addTransaction(true, addr);
             outstanding_access_map.insert(std::make_pair(addr, std::make_pair(n,c)));
          }
       }
       else {
-         std::cout << "Node [" << n->instr_name << "] is processing now\n";
+         std::cout << "Node [" << n->name << "] is processing now\n";
       }
-
       int lat = c->latency_map.at(n);
-      lat--;
-      if ( lat > 0 ) { // node NOT done yet -> in progress 
+      if(lat > 0)
+         lat--;
+      if ( lat > 0  || lat == -1) { // node NOT done yet -> in progress 
+
          c->latency_map.at(n) = lat;
          next_active_list.push_back(n);  // Node <n> will continue execution in next cycle
       }
-      else  { // node done!!
-         std::cout << "Node [" << n->instr_name << "] finished execution \n";
+      else if (lat == 0) { // node done!!
+         std::cout << "Node [" << n->name << "] finished execution \n";
          c->latency_map.erase(n);
          c->dep_map.erase(n);
          c->count++;
@@ -112,7 +79,7 @@ void process_context(Context *c)
             if(it->type == data_dep) {
                // insert dependent d in a map (if not present yet)
                if (c->dep_map.find(d) == c->dep_map.end())  
-                   c->dep_map.insert( std::make_pair(d, d->parent_count) );
+                   c->dep_map.insert( std::make_pair(d, d->parents) );
                
                // decrease # of parents of dependent d
                c->dep_map.at(d)--;
@@ -121,20 +88,23 @@ void process_context(Context *c)
                if (c->dep_map.at(d) == 0 ) {
                   next_active_list.push_back(d);
                   next_newly_active_nodes.insert(d);
-                  std::cout << "Node [" << d->instr_name << "] will start Execution \n";
-                  c->latency_map.insert( std::make_pair(d, d->instr_lat) );
+                  std::cout << "Node [" << d->name << "] will start Execution \n";
+                  c->latency_map.insert( std::make_pair(d, d->lat) );
                   c->dep_map.erase(d);
                }
             }
-            else if(it->type == phi_dep && peek_next_bb() == d->bb_id) {
+            else if(it->type == phi_dep) {// && getNextBB() == d->bid) {
                // do the same for the right context
             }
          }
       }
+      else
+         assert(false);
    }
    c->newly_active_nodes = next_newly_active_nodes;
    c->active_list = next_active_list;
-   if(c->count == bb_map.at(c->bbid)->inst.size()) {
+   if(c->count == (g.bbs.at(c->bid)->inst_count+1)) {
+      std::cout << "Finished after executing " << c->count << " instructions \n";
       c->live = false;
    }
 }
@@ -159,11 +129,8 @@ bool process_cycle()
    return simulate;
 }
 
-// ------------------------------------------------------------------------------------------
-
 int main(int argc, char const *argv[])
 {
-   Graph g;
    DRAMSimCallBack cb;
    DRAMSim::TransactionCompleteCB *read_cb = new DRAMSim::Callback<DRAMSimCallBack, void, unsigned, uint64_t, uint64_t>(&cb, &DRAMSimCallBack::read_complete);
    DRAMSim::TransactionCompleteCB *write_cb = new DRAMSim::Callback<DRAMSimCallBack, void, unsigned, uint64_t, uint64_t>(&cb, &DRAMSimCallBack::write_complete);
@@ -172,31 +139,19 @@ int main(int argc, char const *argv[])
    mem->setCPUClockSpeed(2000000000);
 
    // initilize the latencies array (move to hpp)
-   int lats[10];
-   lats[ADD] = 1;
-   lats[SUB] = 1;
-   lats[LOGICAL] = 1;
-   lats[MULT] = 5;
-   lats[DIV] = 10;
-   lats[LD] = 999999; //3;
-   lats[ST] = 3;
-   lats[BR_COND] = 2;
-   lats[BR_UNCOND] = 2;
 
    Node *nodes[10];
-   int instr_id = 1;
-
-   // create a BB graph
+   int id = 1;
 
    // first, create ALL the nodes
-   
-   nodes[0] = g.addNode();  // this is the entry point
-   nodes[1] = g.addNode(instr_id++, lats[ADD], ADD, "1-add $1,$3,$4", 0);
-   nodes[2] = g.addNode(instr_id++, lats[LD], LD, "2-LD $1,$3,$4", 0);
-   nodes[3] = g.addNode(instr_id++, lats[LOGICAL], LOGICAL, "3-xor $1,$3,$4", 0);
-   nodes[4] = g.addNode(instr_id++, lats[DIV], DIV, "4-mult $1,$3,$4", 0);
-   nodes[5] = g.addNode(instr_id++, lats[SUB], SUB, "5-sub $1,$3,$4", 0);
-   nodes[6] = g.addNode(instr_id++, lats[LOGICAL], LOGICAL, "6-xor $1,$3,$4", 0);
+   g.addBasicBlock(0);
+   nodes[0] = g.addNode(0);  // this is the entry point
+   nodes[1] = g.addNode(id++, ADD,0, "1-add $1,$3,$4");
+   nodes[2] = g.addNode(id++, LD, 0,"2-LD $1,$3,$4");
+   nodes[3] = g.addNode(id++, LOGICAL, 0,"3-xor $1,$3,$4");
+   nodes[4] = g.addNode(id++, DIV, 0,"4-mult $1,$3,$4");
+   nodes[5] = g.addNode(id++, SUB, 0,"5-sub $1,$3,$4");
+   nodes[6] = g.addNode(id++, LOGICAL, 0,"6-xor $1,$3,$4");
 
    // add some dependents
    nodes[0]->addDependent(nodes[1], /*type*/ data_dep);
@@ -208,14 +163,11 @@ int main(int argc, char const *argv[])
    nodes[2]->addDependent(nodes[5], /*type*/ data_dep);
    nodes[2]->addDependent(nodes[6], /*type*/ data_dep);
    nodes[6]->addDependent(nodes[4], /*type*/ data_dep);
-   bb_map.insert(std::make_pair(0, new BasicBlock(0)));
-   for(int i=0; i<7; i++)
-      bb_map.at(0)->addInst(nodes[i]);
-
+  
    cycle_count = 0;
-   context_list.push_back(new Context(recent_context_id++, 0));
+   context_list.push_back(new Context(curr_context_id++, 0));
    context_list.at(0)->active_list.push_back(nodes[0]);
-   context_list.at(0)->latency_map.insert(std::make_pair(nodes[0], nodes[0]->instr_lat));
+   context_list.at(0)->latency_map.insert(std::make_pair(nodes[0], nodes[0]->lat));
    bool simulate = true;
    while (simulate) {
       simulate = process_cycle();
@@ -241,7 +193,7 @@ int main(int argc, char const *argv[])
 g.make_topological_sort( Stack );
 cout << "Topological Sort=";
 while ( ! Stack.empty() ) {
-  cout << Stack.top()->instr_name << " ";
+  cout << Stack.top()->name << " ";
   Stack.pop();
 }
 cout << endl << endl;*/
--- sim/sim-apollo.hpp
+++ sim/sim-apollo.hpp
@@ -1,45 +1,52 @@
-//=======================================================================
-//
-// Project Apollo - simulator
-// Authors: 
-//=======================================================================
-
-// Header file
-
 #include <iostream>                         
 #include <set>
+#include <map>
+#include <vector>
 #include <stack>
 #include <algorithm>
 #include <iterator> 
+#include "assert.h"
 
 namespace apollo {
 class Node;
-typedef enum {NAI, ADD, SUB, LOGICAL, MULT, DIV, LD, ST, BR_COND, BR_UNCOND, PHI} TInstr;
+typedef enum {NAI, ADD, SUB, LOGICAL, MULT, DIV, LD, ST, TERMINATOR, PHI} TInstr;
 typedef enum {data_dep, always_mem_dep, maybe_mem_dep, cf_dep, phi_dep} TEdge;
+
+class Context
+{
+public:
+   bool live;
+   int id;
+   int bid;
+   int count;
+   Context(int id, int bid) : live(true), id(id), bid(bid), count(0) {}
+   std::vector<Node*> active_list;
+   std::set<Node*> newly_active_nodes;
+   std::map<Node*, int> latency_map;
+   std::map<Node*, int> dep_map;
+};
 
 class BasicBlock {
 public:
    int id;
+   int inst_count;
    std::vector<Node*> inst;   
-   BasicBlock(int id) : id(id) {}
+   BasicBlock(int id) : id(id), inst_count(0) {}
    void addInst(Node* n) {
       inst.push_back(n);
+      inst_count++;
    }
 };
 
-// TJH: Probably won't really need edge class 
 class Edge {
 public:
    TEdge type;
    Node *src;   
    Node *dst;
-
    Edge(Node *s, Node *d, TEdge type) : src(s), dst(d), type(type) {}
-   
    bool operator< (const Edge &e) const {         
       return (this < &e);
    }
-   
    bool operator== (const Edge &e) const {
       return ( this->src == e.src && this->dst == e.dst );
    }
@@ -48,143 +55,132 @@ public:
 class Node {
 public:
    std::set<Edge> dependents;    // set of dependent nodes (defined as Edges) 
-   int parent_count;
-   enum {instr, special} type;   // indicates the "type" of node
-   int instr_id;
-   int instr_lat;
-   TInstr instr_type;
-   std::string instr_name;
-   bool visited;
-   int bb_id;
+   int id;
+   int lat;
+   TInstr type;
+   int bid;
+   std::string name;
+   int parents;
+   
+   Node(int id, TInstr type, int bid, std::string name): id(id), type(type), bid(bid), name(name), parents(0)  {
+      lat = getLatency(type);
+   }            
+   Node(int bid) : id(-1), lat(0), type(NAI), bid(bid), name("default"),  parents(0) {}
 
-   // Constructor for an "instruction"-type Node -----------------------------
-   Node(int id, int lat, TInstr type, std::string name, int bb_id):
-            parent_count(0), type(instr), instr_id(id), instr_lat(lat), instr_type(type), 
-            instr_name(name), visited(false), bb_id(bb_id) {}
-            
-   // Constructor for a "special" Node (ie, a BB's entry/exit point)----------
-   Node() : parent_count(0), type(special), instr_id(0), instr_lat(0), instr_type(NAI), 
-            instr_name("special"), visited(false), bb_id(-1) {}
-
-   // -----------------------------------------------------------------------
+   int getLatency(TInstr type) {
+      int lat = -1;
+      switch(type) {
+         case ADD:
+         case SUB:
+         case LOGICAL:
+            lat = 1;
+            break;
+         case MULT:
+            lat = 3;
+            break;
+         case DIV:
+            lat = 9;
+            break;
+         case LD:
+            lat = -1;
+            break;
+         case ST:
+            lat = 3;
+            break;
+         case TERMINATOR:
+            lat = 1;
+            break;
+         case PHI:
+            lat = 0;
+            break;
+         case NAI:
+            lat = 0;
+            break;
+         default:
+            assert(false);
+            break;
+      }
+      return lat;
+   }
    void addDependent(Node *dest, TEdge type) {
       Edge e(this, dest, type);
       dependents.insert(e); 
-      dest->parent_count++; 
+      dest->parents++; 
    }
 
-   // -----------------------------------------------------------------------
    void eraseDependent(Node *dest, TEdge type) {
       std::set<Edge>::iterator it;
       it = std::find( dependents.begin(), dependents.end(), Edge(this,dest,type) );
       if ( it != dependents.end() ) {  // found
          dependents.erase(*it);
-         dest->parent_count--; 
+         dest->parents--; 
       }
    }
+   // FIX
    /*
    // Outputing a Node -----------------------------------------------------------
    friend std::ostream &operator<<(std::ostream &os, Node &n) {
-      os << "I[" << n.instr_name << "], lat=" << n.instr_lat << ", Deps = {";
+      os << "I[" << n.name << "], lat=" << n.lat << ", Deps = {";
       std::set<Edge>::iterator it;
       for (  it = n.dependents.begin(); it != n.dependents.end(); ++it )
-         std::cout << "[" << it->dst->instr_name << "], ";
+         std::cout << "[" << it->dst->name << "], ";
       std::cout << "}";
    }
-
-   // Recursive function for Depth First Search ------------------------------------
-   int calculate_accum_latency_from_me() {
-      int lat = 0;
-      
-      if ( !visited ) {
-         visited = true;
-         lat = instr_lat;
-         
-         // Recursively traverse ALL dependents
-         std::set<Edge>::iterator it;
-         for ( it = dependents.begin(); it != dependents.end(); ++it )
-            lat += it->dst->calculate_accum_latency_from_me();
-      }
-      return lat;
-   }
-
-   // Recursive function for doing a Topological Sort traversal -----------------------
-   void topologicalSort(std::stack<Node *> &Stack) {
-      
-      if ( !visited ) {
-         visited = true;
-
-         // Recursively traverse ALL dependents
-         std::set<Edge>::iterator it;
-         for (  it = dependents.begin(); it != dependents.end(); ++it )
-            if ( !visited ) {
-               it->dst->topologicalSort(Stack); std::cout << " a ";
-            }
-
-         // Push current Node into the stack
-         Stack.push(this);        
-      }
-   }*/
+   */
 };
 
 // ******************************************************************************
 class Graph {
 public:
-   std::set<Node *> nodes;
-
+   std::map<int, Node *> nodes;
+   std::map<int, BasicBlock*> bbs;
    int get_num_nodes() { return nodes.size(); }
    int size() { return nodes.size(); }
 
-   // Destructor -----------------------------------------------------------------
    ~Graph() { eraseAllNodes(); } 
 
+   void addBasicBlock(int id) {
+       bbs.insert(std::make_pair(id, new BasicBlock(id)));
+   }
    // Adding an "instruction"-type Node ------------------------------------------
-   Node *addNode(int id, int lat, TInstr type, std::string name, int bid) {
-      Node *n = new Node(id, lat, type, name, bid);
-      nodes.insert(n);
+   Node *addNode(int id, TInstr type, int bid, std::string name) {
+      Node *n = new Node(id, type, bid, name);
+      nodes.insert(std::make_pair(n->id, n));
+      if(bbs.find(bid) == bbs.end())
+         assert(false);
+      bbs.at(bid)->addInst(n);
       return n;
    }
-
    // Adding a "special"-type Node -----------------------------------------------
-   Node *addNode() {
-      Node *n = new Node;
-      nodes.insert(n);
+   Node *addNode(int bid) {
+      Node *n = new Node(bid);
+      nodes.insert(std::make_pair(-1, n));
       return n;
    }
-
-   // Return an exsisting NODE given an <instr_id> ---------------------------------
    Node *getNode(int id) {
-      // search the Node with <instr_id> == <id>
-      for ( std::set<Node *>::iterator it = nodes.begin(); it != nodes.end(); ++it )
-         if ( (*it)->instr_id == id )
-            return *it;
-      return NULL;  // not found -> but this should not happen !!!
+      if(nodes.find(id) != nodes.end())
+         return nodes.at(id);
+      else
+         return NULL;
    }
-
-   // --------------------------------------------------------------------------------
    void eraseNode(Node *n) { 
       if (n) {
-         nodes.erase( n ); 
+         nodes.erase(n->id); 
          delete n;
       }
    }
-
-   // --------------------------------------------------------------------------------
    void eraseAllNodes() { 
-      for ( std::set<Node *>::iterator it = nodes.begin(); it != nodes.end(); ++it )
-         eraseNode(*it);
+      for ( std::map<int, Node *>::iterator it = nodes.begin(); it != nodes.end(); ++it )
+         eraseNode(it->second);
    }
 
-   // --------------------------------------------------------------------------------
    void addDependent(Node *src, Node *dest, TEdge type) {
       src->addDependent(dest, type);
    }
-
-   // --------------------------------------------------------------------------------
    void eraseDependent(Node *src, Node *dest, TEdge type) {
       src->eraseDependent(dest, type);
    }
-
+   // FIX
    /* 
    // Outputing a Graph --------------------------------------------------------------
    friend std::ostream &operator<<(std::ostream &os, Graph &g) {
@@ -194,31 +190,7 @@ public:
       for ( it = g.nodes.begin(); it != g.nodes.end(); ++it )
          std::cout << "node_" << i++ << ": " << **it << std::endl;
       std::cout << "";
-   }
-
-   // --------------------------------------------------------------------------------
-   void clear_all_visits() {
-      for ( std::set<Node *>::iterator it = nodes.begin(); it != nodes.end(); ++it )
-         (*it)->visited = false;
-   }
-
-   // Graph's accumulated latency by doing a DFS search -----------------------------------
-   int calculate_accum_latency() {
-      clear_all_visits();
-      Node *n = (*nodes.begin()); // FIXME: maybe we need to locate "a correct entry point"
-      return n->calculate_accum_latency_from_me();
-   }
-
-   // Make a Topological Sorting ----------------------------------------------------------
-   void make_topological_sort( std::stack<Node *> &Stack ) {
-      clear_all_visits();
-      
-      // traverse ALL nodes one by one AND call the recursive helper function
-      std::set<Node *>::iterator it;
-      for (  it = nodes.begin(); it != nodes.end(); ++it )
-         if ( ! (*it)->visited )
-            (*it)->topologicalSort(Stack);
-   }*/
+   } 
+   */
 };
-
 }
