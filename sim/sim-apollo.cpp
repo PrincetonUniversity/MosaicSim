7620e22aecab082d65eb1ea82c42c65e2424d24b
Tae Jun <ham.taejun@gmail.com>
2018-06-14 03:01:01 -0400

    further update

--- sim/sim-apollo.cpp
+++ sim/sim-apollo.cpp
@@ -12,16 +12,6 @@ using namespace std;
 #include <sstream> 
 #include <cstdio> 
 
-Graph g;
-int cycle_count = 0;
-int curr_context_id = 0;
-std::vector<Context*> context_list;
-std::map<uint64_t, std::pair<Node*,Context*> > outstanding_access_map; 
-std::map<int, std::map<Node *, int> > future_contexts; // context id, list of nodes, processed inter-context dependency
-std::vector<int> cf; // List of basic blocks in a program order
-DRAMSim::MultiChannelMemorySystem *mem;
-// TODO: Memory address overlap across contexts
-// TODO: Handle 0-latency instructions correctly
 vector<string> split(const string &s, char delim) {
     stringstream ss(s);
     string item;
@@ -31,213 +21,236 @@ vector<string> split(const string &s, char delim) {
     }
     return tokens;
 }
-void readCF() {
-   string line;
-   ifstream cfile ("ctrl.txt");
-   int last_bbid = -1;
-   if (cfile.is_open()) {
-    while (getline (cfile,line)) {
-      vector<string> s = split(line, ',');
-      if(s.size() != 3)
-         assert(false);
-      if(stoi(s.at(1)) != last_bbid && last_bbid != -1)
-         assert(false);
-      last_bbid = stoi(s.at(1));
-      cf.push_back(stoi(s.at(2)));
-    }
-  }
-  cfile.close();
-}
-class DRAMSimCallBack {
-   public: 
-      void read_complete(unsigned id, uint64_t address, uint64_t mem_clock_cycle) {
-         printf("[DRAM] Memory Read Complete. id:%d addr:%lu mem_cycle:%lu\n", id, address, mem_clock_cycle);
-         if ( outstanding_access_map.find(address) == outstanding_access_map.end() )
+
+class Simulator
+{ 
+public:
+   // TODO: Memory address overlap across contexts
+   // TODO: Handle 0-latency instructions correctly
+   class DRAMSimCallBack {
+      public: 
+         std::map<uint64_t, std::pair<Node*,Context*> > outstanding_access_map;
+         void read_complete(unsigned id, uint64_t address, uint64_t mem_clock_cycle) {
+            printf("[DRAM] Memory Read Complete. id:%d addr:%lu mem_cycle:%lu\n", id, address, mem_clock_cycle);
+            if ( outstanding_access_map.find(address) == outstanding_access_map.end() )
+               assert(false);
+            Node *n = outstanding_access_map.at(address).first;
+            Context *c = outstanding_access_map.at(address).second;
+            c->remaining_cycles_map.at(n) = 0; // load marked as DONE !
+            outstanding_access_map.erase(address);
+         }
+         void write_complete(unsigned id, uint64_t address, uint64_t clock_cycle) {
+            printf("[DRAM] Memory Write Complete: %d %lu cycle=%lu\n", id, address, clock_cycle);
+            if( outstanding_access_map.find(address) == outstanding_access_map.end() )
+               assert(false);
+            outstanding_access_map.erase(address);
+         }
+   };
+
+   Graph g;
+   int cycle_count = 0;
+   int curr_context_id = 0;
+   std::vector<Context*> context_list;
+   std::vector<int> cf; // List of basic blocks in a program order 
+   DRAMSimCallBack cb;
+   std::map<int, std::map<Node *, int> > future_contexts; // context id, list of nodes, processed inter-context dependency
+   DRAMSim::MultiChannelMemorySystem *mem;
+
+   void initalize() {
+      DRAMSim::TransactionCompleteCB *read_cb = new DRAMSim::Callback<DRAMSimCallBack, void, unsigned, uint64_t, uint64_t>(&cb, &DRAMSimCallBack::read_complete);
+      DRAMSim::TransactionCompleteCB *write_cb = new DRAMSim::Callback<DRAMSimCallBack, void, unsigned, uint64_t, uint64_t>(&cb, &DRAMSimCallBack::write_complete);
+      mem = DRAMSim::getMemorySystemInstance("sim/dramsim2/ini/DDR3_micron_16M_8B_x8_sg15.ini", "sim/dramsys.ini", "..", "Apollo", 16384); 
+      mem->RegisterCallbacks(read_cb, write_cb, NULL);
+      mem->setCPUClockSpeed(2000000000);  
+   }
+   void readCF() {
+      string line;
+      ifstream cfile ("ctrl.txt");
+      int last_bbid = -1;
+      if (cfile.is_open()) {
+       while (getline (cfile,line)) {
+         vector<string> s = split(line, ',');
+         if(s.size() != 3)
             assert(false);
-         Node *n = outstanding_access_map.at(address).first;
-         Context *c = outstanding_access_map.at(address).second;
-         c->remaining_cycles_map.at(n) = 0; // load marked as DONE !
-         outstanding_access_map.erase(address);
-      }
-      void write_complete(unsigned id, uint64_t address, uint64_t clock_cycle) {
-         printf("[DRAM] Memory Write Complete: %d %lu cycle=%lu\n", id, address, clock_cycle);
-         if( outstanding_access_map.find(address) == outstanding_access_map.end() )
+         if(stoi(s.at(1)) != last_bbid && last_bbid != -1)
             assert(false);
-         outstanding_access_map.erase(address);
+         last_bbid = stoi(s.at(1));
+         cf.push_back(stoi(s.at(2)));
+       }
+     }
+     cfile.close();
+   }
+
+   void readGraph()
+   {
+      Node *nodes[10];  
+      int id = 1;
+      g.addBasicBlock(0);
+      nodes[1] = g.addNode(id++, ADD, 0, "1-add $1,$3,$4");
+      nodes[2] = g.addNode(id++, LD, 0,"2-LD $1,$3,$4");
+      nodes[3] = g.addNode(id++, LOGICAL, 0,"3-xor $1,$3,$4");
+      nodes[4] = g.addNode(id++, DIV, 0,"4-mult $1,$3,$4");
+      nodes[5] = g.addNode(id++, SUB, 0,"5-sub $1,$3,$4");
+      nodes[6] = g.addNode(id++, LOGICAL, 0,"6-xor $1,$3,$4");
+
+      // add some dependents
+      nodes[1]->addDependent(nodes[2], /*type*/ data_dep);
+      nodes[1]->addDependent(nodes[3], /*type*/ data_dep);
+      nodes[1]->addDependent(nodes[4], /*type*/ data_dep);
+      nodes[2]->addDependent(nodes[5], /*type*/ data_dep);
+      nodes[2]->addDependent(nodes[6], /*type*/ data_dep);
+      nodes[6]->addDependent(nodes[4], /*type*/ data_dep);
+      cout << g;
+      createContext(g.bbs.at(0));
+   }
+
+   Context* createContext(BasicBlock *bb)
+   {
+      int cid = curr_context_id;
+      context_list.push_back(new Context(cid));
+      curr_context_id++;
+      context_list.at(cid)->initialize(bb);
+      if(future_contexts.find(cid) != future_contexts.end()) {
+         context_list.at(cid)->updateDependency(future_contexts.at(cid));
+         future_contexts.erase(cid);
       }
+      return context_list.at(cid);
+   }
+   void updateFutureDependent(int cid, Node *d)
+   {
+      if(future_contexts.find(cid) != future_contexts.end() && future_contexts.at(cid).find(d) != future_contexts.at(cid).end())
+         future_contexts.at(cid).at(d)++;
+      else {
+         future_contexts[cid][d]=1;
+      }
+   }
+   void updateDependent(Context *c, Node *d)
+   {
+      if (c->pending_parents_map.find(d) == c->pending_parents_map.end())  
+         assert(false);
+      
+      // decrease # of pending ancestors of dependent <d>
+      c->pending_parents_map.at(d)--;
+
+      // if <d> has no more pending ancestors -> push it for "execution" in next cycle
+      if ( c->pending_parents_map.at(d) == 0 ) {
+         c->next_active_list.push_back(d);
+         c->next_start_set.insert(d);
+         std::cout << "Node [" << d->name << "]: Ready to Execute\n";
+         c->remaining_cycles_map.insert( std::make_pair(d, d->lat) );
+      }
+   }
+   void process_context(Context *c)
+   {
+      // process ALL nodes in <active_list>, ie, those being executed in this cycle
+      for ( int i=0; i < c->active_list.size(); i++ ) {
+
+         Node *n = c->active_list.at(i);
+         if ( c->start_set.find(n) != c->start_set.end() ) {
+            
+            std::cout << "Node [" << n->name << "]: Starts Execution \n";
+            // Memory instructions treatment
+            if (n->type == LD || n->type == ST) {
+               uint64_t addr = n->id * 64; // TODO: Use Real Address
+               std::cout << "Node [" << n->name << "]: Inserts Memory Transaction for Address "<< addr << "\n";
+               if ( !mem->willAcceptTransaction(addr) )
+                  assert(false);
+               if ( n->type == LD ) {
+                  mem->addTransaction(false, addr);
+                  cb.outstanding_access_map.insert( std::make_pair(addr, std::make_pair(n,c)) );
+               }
+               else if ( n->type == ST ) {
+                  mem->addTransaction(true, addr);
+                  cb.outstanding_access_map.insert( std::make_pair(addr, std::make_pair(n,c)) );
+               }
+            }
+         }
+         else 
+            std::cout << "Node [" << n->name << "]: Processing \n";
+
+         // decrease the remaining # of cycles for current node <n>
+         int remainig_cycles = c->remaining_cycles_map.at(n);
+         if (remainig_cycles > 0)
+           remainig_cycles--;
+         if ( remainig_cycles > 0 || remainig_cycles == -1 ) {  // Node <n> will continue execution in next cycle 
+            // A remaining_cycles == -1 represents a outstanding memory request being processed currently by DRAMsim
+            c->remaining_cycles_map.at(n) = remainig_cycles;
+            c->next_active_list.push_back(n);  
+         }
+         else if (remainig_cycles == 0) { // Execution Finished for node <n>
+            std::cout << "Node [" << n->name << "]: Finished Execution \n";
+
+            c->remaining_cycles_map.erase(n);
+            if ( n->type != NAI )
+               c->processed++;
+
+            // Update Dependents
+            std::set< std::pair<Node*, TEdge> >::iterator it;
+            for ( it = n->dependents.begin(); it != n->dependents.end(); ++it ) {
+               Node *d = it->first;
+               TEdge t = it->second;
+               
+               if (t == data_dep || t == bb_dep) {
+                  updateDependent(c, d);
+               }
+               else if (t == phi_dep  && (cf.at(c->id+1) == d->bbid)) {
+                  if(context_list.size() > c->id +1)
+                     updateDependent(context_list.at(c->id+1),d);
+                  else
+                     updateFutureDependent(c->id+1, d);
+               }
+            }
+         }
+         else
+            assert(false);
+      }
+      // Continue with following active instructions
+      c->start_set = c->next_start_set;
+      c->active_list = c->next_active_list;
+      c->next_start_set.clear();
+      c->next_active_list.clear();
+      // Check if current context is done
+      if ( c->processed == g.bbs.at(c->bbid)->inst_count ) {
+         std::cout << "Context [" << c->id << "]: Finished Execution (Executed " << c->processed << " instructions) \n";
+         c->live = false;
+      }
+   }
+   void process_memory()
+   {
+      mem->update();
+   }
+   bool process_cycle()
+   {
+      std::cout << "Cycle: " << cycle_count << "\n";
+      cycle_count++;
+      bool simulate = false;
+
+      if (cycle_count > 500)
+         assert(false);
+      for (int i=0; i<context_list.size(); i++) {
+         process_context( context_list.at(i) );
+         if ( context_list.at(i)->live )
+            simulate = true;
+      }
+      process_memory();
+      return simulate;
+   }
+   void run()
+   {
+      bool simulate = true;
+      while (simulate) {
+         simulate = process_cycle();
+      }
+      mem->printStats(false);
+   }
 };
 
-void updateFutureDependent(int cid, Node *d)
-{
-   if(future_contexts.find(cid) != future_contexts.end() && future_contexts.at(cid).find(d) != future_contexts.at(cid).end())
-      future_contexts.at(cid).at(d)++;
-   else {
-      future_contexts[cid][d]=1;
-   }
-}
-void updateDependent(Context *c, Node *d)
-{
-   if (c->pending_parents_map.find(d) == c->pending_parents_map.end())  
-      assert(false);
-   
-   // decrease # of pending ancestors of dependent <d>
-   c->pending_parents_map.at(d)--;
-
-   // if <d> has no more pending ancestors -> push it for "execution" in next cycle
-   if ( c->pending_parents_map.at(d) == 0 ) {
-      c->next_active_list.push_back(d);
-      c->next_start_set.insert(d);
-      std::cout << "Node [" << d->name << "]: Ready to Execute\n";
-      c->remaining_cycles_map.insert( std::make_pair(d, d->lat) );
-   }
-}
-void process_context(Context *c)
-{
-   // process ALL nodes in <active_list>, ie, those being executed in this cycle
-   for ( int i=0; i < c->active_list.size(); i++ ) {
-
-      Node *n = c->active_list.at(i);
-      if ( c->start_set.find(n) != c->start_set.end() ) {
-         
-         std::cout << "Node [" << n->name << "]: Starts Execution \n";
-         // Memory instructions treatment
-         if (n->type == LD || n->type == ST) {
-            uint64_t addr = n->id * 64; // TODO: Use Real Address
-            std::cout << "Node [" << n->name << "]: Inserts Memory Transaction for Address "<< addr << "\n";
-            if ( !mem->willAcceptTransaction(addr) )
-               assert(false);
-            if ( n->type == LD ) {
-               mem->addTransaction(false, addr);
-               outstanding_access_map.insert( std::make_pair(addr, std::make_pair(n,c)) );
-            }
-            else if ( n->type == ST ) {
-               mem->addTransaction(true, addr);
-               outstanding_access_map.insert( std::make_pair(addr, std::make_pair(n,c)) );
-            }
-         }
-      }
-      else 
-         std::cout << "Node [" << n->name << "]: Processing \n";
-
-      // decrease the remaining # of cycles for current node <n>
-      int remainig_cycles = c->remaining_cycles_map.at(n);
-      if (remainig_cycles > 0)
-        remainig_cycles--;
-      if ( remainig_cycles > 0 || remainig_cycles == -1 ) {  // Node <n> will continue execution in next cycle 
-         // A remaining_cycles == -1 represents a outstanding memory request being processed currently by DRAMsim
-         c->remaining_cycles_map.at(n) = remainig_cycles;
-         c->next_active_list.push_back(n);  
-      }
-      else if (remainig_cycles == 0) { // Execution Finished for node <n>
-         std::cout << "Node [" << n->name << "]: Finished Execution \n";
-
-         c->remaining_cycles_map.erase(n);
-         if ( n->type != NAI )
-            c->processed++;
-
-         // Update Dependents
-         std::set< std::pair<Node*, TEdge> >::iterator it;
-         for ( it = n->dependents.begin(); it != n->dependents.end(); ++it ) {
-            Node *d = it->first;
-            TEdge t = it->second;
-            
-            if (t == data_dep || t == bb_dep) {
-               updateDependent(c, d);
-            }
-            else if (t == phi_dep  && (cf.at(c->id+1) == d->bbid)) {
-               if(context_list.size() > c->id +1)
-                  updateDependent(context_list.at(c->id+1),d);
-               else
-                  updateFutureDependent(c->id+1, d);
-            }
-         }
-      }
-      else
-         assert(false);
-   }
-   // Continue with following active instructions
-   c->start_set = c->next_start_set;
-   c->active_list = c->next_active_list;
-   c->next_start_set.clear();
-   c->next_active_list.clear();
-   // Check if current context is done
-   if ( c->processed == g.bbs.at(c->bbid)->inst_count ) {
-      std::cout << "Context [" << c->id << "]: Finished Execution (Executed " << c->processed << " instructions) \n";
-      c->live = false;
-   }
-}
-void process_memory()
-{
-   mem->update();
-}
-
-bool process_cycle()
-{
-   std::cout << "Cycle: " << cycle_count << "\n";
-   cycle_count++;
-   bool simulate = false;
-
-   if (cycle_count > 500)
-      assert(false);
-   for (int i=0; i<context_list.size(); i++) {
-      process_context( context_list.at(i) );
-      if ( context_list.at(i)->live )
-         simulate = true;
-   }
-   process_memory();
-   return simulate;
-}
-Context* createContext(BasicBlock *bb)
-{
-   int cid = curr_context_id;
-   context_list.push_back(new Context(cid));
-   curr_context_id++;
-   context_list.at(cid)->initialize(bb);
-   if(future_contexts.find(cid) != future_contexts.end()) {
-      context_list.at(cid)->updateDependency(future_contexts.at(cid));
-      future_contexts.erase(cid);
-   }
-   return context_list.at(cid);
-}
-
-void readGraph()
-{
-   Node *nodes[10];  
-   int id = 1;
-
-   g.addBasicBlock(0);
-   nodes[1] = g.addNode(id++, ADD, 0, "1-add $1,$3,$4");
-   nodes[2] = g.addNode(id++, LD, 0,"2-LD $1,$3,$4");
-   nodes[3] = g.addNode(id++, LOGICAL, 0,"3-xor $1,$3,$4");
-   nodes[4] = g.addNode(id++, DIV, 0,"4-mult $1,$3,$4");
-   nodes[5] = g.addNode(id++, SUB, 0,"5-sub $1,$3,$4");
-   nodes[6] = g.addNode(id++, LOGICAL, 0,"6-xor $1,$3,$4");
-
-   // add some dependents
-   nodes[1]->addDependent(nodes[2], /*type*/ data_dep);
-   nodes[1]->addDependent(nodes[3], /*type*/ data_dep);
-   nodes[1]->addDependent(nodes[4], /*type*/ data_dep);
-   nodes[2]->addDependent(nodes[5], /*type*/ data_dep);
-   nodes[2]->addDependent(nodes[6], /*type*/ data_dep);
-   nodes[6]->addDependent(nodes[4], /*type*/ data_dep);
-   cout << g;
-}
 int main(int argc, char const *argv[])
 {
-   DRAMSimCallBack cb;
-   DRAMSim::TransactionCompleteCB *read_cb = new DRAMSim::Callback<DRAMSimCallBack, void, unsigned, uint64_t, uint64_t>(&cb, &DRAMSimCallBack::read_complete);
-   DRAMSim::TransactionCompleteCB *write_cb = new DRAMSim::Callback<DRAMSimCallBack, void, unsigned, uint64_t, uint64_t>(&cb, &DRAMSimCallBack::write_complete);
-   mem = DRAMSim::getMemorySystemInstance("sim/dramsim2/ini/DDR3_micron_16M_8B_x8_sg15.ini", "sim/dramsys.ini", "..", "Apollo", 16384); 
-   mem->RegisterCallbacks(read_cb, write_cb, NULL);
-   mem->setCPUClockSpeed(2000000000);
-   readGraph();
-   readCF();
-   cycle_count = 0;
-   createContext(g.bbs.at(0));
-   bool simulate = true;
-   while (simulate) {
-      simulate = process_cycle();
-   }
-   mem->printStats(false);  
+   Simulator sim;
+   sim.readGraph();
+   sim.readCF();
+   sim.initalize();
+   sim.run();
    return 0;
 } 
