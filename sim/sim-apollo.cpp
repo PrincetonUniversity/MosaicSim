d5851534dd46337457a6ca1c0414f5aacf0759ed
juanlaragon <jlaragon@cafe.CS.Princeton.EDU>
2018-06-25 14:45:08 -0400

    fixings

--- sim/sim-apollo.cpp
+++ sim/sim-apollo.cpp
@@ -25,7 +25,7 @@ public:
 
    Context(int id) : live(true), id(id), bbid(-1), processed(0) {}
 
-  void initialize(BasicBlock *bb, std::map<Node*, std::pair<int, bool> > &curr_owner, std::map<DNode, std::vector<DNode> > &deps, std::map<int, std::set<Node*> > &handled_phi_deps, map<uint64_t,deque<tuple<int,int,bool,bool>>>& load_queue_map, map<uint64_t,deque<tuple<int,int,bool,bool>>>& store_queue_map, map<int, queue<uint64_t> >& memory) {
+   void initialize(BasicBlock *bb, std::map<Node*, std::pair<int, bool> > &curr_owner, std::map<DNode, std::vector<DNode> > &deps, std::map<int, std::set<Node*> > &handled_phi_deps, map<uint64_t,deque<tuple<int,int,bool,bool>>>& load_queue_map, map<uint64_t,deque<tuple<int,int,bool,bool>>>& store_queue_map, map<int, queue<uint64_t> >& memory) {
       assert(bbid == -1);
       bbid = bb->id;
       active_list.push_back(bb->entry);
@@ -35,33 +35,33 @@ public:
       for ( int i=0; i<bb->inst.size(); i++ ) {
          Node *n = bb->inst.at(i);
 
-	 if (n->type == ST){
-	     //Luwa: add to store queue as soon as context is created. this way, there's a serialized ordering of stores in store queue
-	     
-	   deque<tuple<int, int, bool, bool>> addr_queue;
-	   uint64_t addr = memory.at(n->id).front(); //in this context, this is always the only store to be done by node n 
-	   if (store_queue_map.find(addr)!=store_queue_map.end())
-	     {addr_queue=store_queue_map.at(addr);}
-	   //luwa working here
-	   addr_queue.push_back(make_tuple(id, n->id, false, false));
-	   store_queue_map[addr]=addr_queue;
-	 }
-	 
-	 if (n->type == LD){
-	   //Luwa: add to load queue as soon as context is created. this way, there's a serialized ordering of loads in load queue
-	     
-	   deque<tuple<int, int, bool, bool>> addr_queue;
-	   uint64_t addr = memory.at(n->id).front(); //in this context, this is always the only load to be done by node n 
-	   if (load_queue_map.find(addr)!=load_queue_map.end())
-	     {addr_queue=load_queue_map.at(addr);}
-	   //luwa working here
-	   addr_queue.push_back(make_tuple(id, n->id, false, false));
-	   load_queue_map[addr]=addr_queue;
-	 }
-	 
-	 if(n->type == PHI)
+      if (n->type == ST){
+           //Luwa: add to store queue as soon as context is created. this way, there's a serialized ordering of stores in store queue
+           
+         deque<tuple<int, int, bool, bool>> addr_queue;
+         uint64_t addr = memory.at(n->id).front(); //in this context, this is always the only store to be done by node n 
+         if (store_queue_map.find(addr)!=store_queue_map.end())
+           {addr_queue=store_queue_map.at(addr);}
+         //luwa working here
+         addr_queue.push_back(make_tuple(id, n->id, false, false));
+         store_queue_map[addr]=addr_queue;
+      }
+       
+      if (n->type == LD){
+         //Luwa: add to load queue as soon as context is created. this way, there's a serialized ordering of loads in load queue
+           
+         deque<tuple<int, int, bool, bool>> addr_queue;
+         uint64_t addr = memory.at(n->id).front(); //in this context, this is always the only load to be done by node n 
+         if (load_queue_map.find(addr)!=load_queue_map.end())
+           {addr_queue=load_queue_map.at(addr);}
+         //luwa working here
+         addr_queue.push_back(make_tuple(id, n->id, false, false));
+         load_queue_map[addr]=addr_queue;
+      }
+    
+      if(n->type == PHI)
             pending_parents_map.insert(std::make_pair(n, n->parents.size()+1));
-	 	   
+         
          else
             pending_parents_map.insert(std::make_pair(n, n->parents.size()));
          pending_external_parents_map.insert(std::make_pair(n, n->external_parents.size()));
@@ -145,9 +145,9 @@ public:
             cout << "Node [" << n->name << " @ context " << c->id << "]: Read Transaction Returns "<< addr << "\n";
             if (c->remaining_cycles_map.find(n) == c->remaining_cycles_map.end())
                cout << *n << " / " << c->id << "\n";
-	    
-	    c->remaining_cycles_map.at(n) = 0; // mark "Load" as finished for sim-apollo
-	    sim->update_memop_queue(sim->load_queue_map,addr,make_tuple(c->id,n->id,true,true)); //set entry in load queue to finished
+       
+       c->remaining_cycles_map.at(n) = 0; // mark "Load" as finished for sim-apollo
+       sim->update_memop_queue(sim->load_queue_map,addr,make_tuple(c->id,n->id,true,true)); //set entry in load queue to finished
             q.pop();
             if (q.size() == 0)
                outstanding_accesses_map.erase(addr);
@@ -157,12 +157,12 @@ public:
             queue<std::pair<Node*, Context*>> &q = outstanding_accesses_map.at(addr);
             Node *n = q.front().first;
             Context *c = q.front().second;
-	                 
-	    c->remaining_cycles_map.at(n) = 0; //Luwa: just added this. don't know why we weren't marking stores as finished
-	    sim->update_memop_queue(sim->store_queue_map,addr,make_tuple(c->id,n->id,true,true)); //set entry in store queue to finished
-	    cout << "Node [" << n->name << " @ context " << c->id << "]: Write Transaction Returns "<< addr << "\n";
+                    
+       c->remaining_cycles_map.at(n) = 0; //Luwa: just added this. don't know why we weren't marking stores as finished
+       sim->update_memop_queue(sim->store_queue_map,addr,make_tuple(c->id,n->id,true,true)); //set entry in store queue to finished
+       cout << "Node [" << n->name << " @ context " << c->id << "]: Write Transaction Returns "<< addr << "\n";
             q.pop();
-	    
+       
             if(q.size() == 0)
                outstanding_accesses_map.erase(addr);
          }
@@ -181,7 +181,7 @@ public:
    
    vector<int> cf; // List of basic blocks in a program order 
    map<int, queue<uint64_t> > memory; // List of memory accesses per instruction in a program order
-  map<int, queue<uint64_t> >& memory_ref=memory;
+   map<int, queue<uint64_t> >& memory_ref=memory;
    /* Handling External Dependencies */
    map<Node*, pair<int, bool> > curr_owner; // Source of external dependency (Node), Context ID for the node (cid), Finished
    map<DNode, vector<DNode> > deps; // Source of external depndency (Node,cid), Destinations for that source (Node, cid)
@@ -202,22 +202,22 @@ public:
       createContext(g.initialBlock);
    }
 
-  void update_memop_queue(map<uint64_t,deque<tuple<int,int,bool,bool>>>& memop_queue_map, uint64_t addr, tuple<int,int,bool,bool> memop_queue_entry){   
-    deque<tuple<int, int, bool, bool>>* addr_queue;
-    if(memop_queue_map.find(addr)==memop_queue_map.end()) { //there's no memop_queue for addr
-      addr_queue->push_back(memop_queue_entry);
-      memop_queue_map[addr]=*addr_queue;
-    }
-    else{   
-      deque<tuple<int,int,bool,bool>>* addr_queue = &memop_queue_map.at(addr);
-      deque<tuple<int,int,bool,bool>>::iterator it = addr_queue->begin();   
-      while(it!=addr_queue->end() && !((get<0>(*it)==get<0>(memop_queue_entry)) && (get<1>(*it)==get<1>(memop_queue_entry))) ){
-	++it;
+   void update_memop_queue(map<uint64_t,deque<tuple<int,int,bool,bool>>>& memop_queue_map, uint64_t addr, tuple<int,int,bool,bool> memop_queue_entry) {   
+      deque<tuple<int, int, bool, bool>>* addr_queue;
+      if(memop_queue_map.find(addr)==memop_queue_map.end()) { //there's no memop_queue for addr
+         addr_queue->push_back(memop_queue_entry);
+         memop_queue_map[addr]=*addr_queue;
       }
-      assert(it!=addr_queue->end()); //there should have been an entry
-      *it=memop_queue_entry;
-    }
-  }
+      else{   
+         deque<tuple<int,int,bool,bool>>* addr_queue = &memop_queue_map.at(addr);
+         deque<tuple<int,int,bool,bool>>::iterator it = addr_queue->begin();   
+         while (it!=addr_queue->end() && !((get<0>(*it)==get<0>(memop_queue_entry)) && (get<1>(*it)==get<1>(memop_queue_entry))) ) {
+            ++it;
+         }
+         assert(it!=addr_queue->end()); //there should have been an entry
+         *it=memop_queue_entry;
+      }
+   }
 
    void createContext(int bid)
    {
@@ -243,83 +243,83 @@ public:
    bool memop_started(tuple<int,int,bool, bool> memop){
       return get<2>(memop);}
 
-  //return pointer to preceeding store if curr_memop is load and vice versa  
-  deque<tuple<int,int,bool,bool>>::iterator most_recent_memop  (deque<tuple<int,int,bool,bool>> memop_queue, tuple<int,int,bool,bool> curr_memop, uint64_t addr){
+   //return pointer to preceeding store if curr_memop is load and vice versa  
+   deque<tuple<int,int,bool,bool>>::iterator most_recent_memop  (deque<tuple<int,int,bool,bool>> memop_queue, tuple<int,int,bool,bool> curr_memop, uint64_t addr) {
       bool older_memop_exists=false;
       std::deque<tuple<int,int,bool,bool>>::iterator it=memop_queue.begin();
-      while(it!=memop_queue.end() && memop_older(*it,curr_memop)){ //continue looping until you reach 1st instruction younger than curr_memop
-	 ++it;
-	 older_memop_exists=true;
+      while(it!=memop_queue.end() && memop_older(*it,curr_memop)) { //continue looping until you reach 1st instruction younger than curr_memop
+         ++it;
+         older_memop_exists=true;
       }
 
       if (it==memop_queue.end() && !older_memop_exists)
-	return it;
+         return it;
       return --it; //return most recent preceeding memop
    }
 
-  void process_context(Context *c)
+   void process_context(Context *c)
    {
       // traverse ALL the active instructions in the context
-           
       for (int i=0; i < c->active_list.size(); i++) {
          Node *n = c->active_list.at(i);
          if (c->start_set.find(n) != c->start_set.end()) {
             cout << "Node [" << n->name << " @ context " << c->id << "]: Starts Execution \n";
             /* check if it's a Memory Request -> enqueue in DRAMsim */
             if (n->type == LD || n->type == ST) {
-	        uint64_t addr = memory.at(n->id).front();  // get the 1st (oldest) memory access for this <id>
-		
+            
+               uint64_t addr = memory.at(n->id).front();  // get the 1st (oldest) memory access for this <id>
+      
                //Luwa: we just move to the next active node and check again in next cycle if mem dependency is resolved
-	       bool can_receive_forward=false;
-	       bool must_stall=false;
-	       tuple<int,int,bool,bool> curr_memop=make_tuple(c->id,n->id,false,false);
-	        
-	       
-	       if( store_queue_map.find(addr)!=store_queue_map.end()) { //store queue for addr exists, otherwise no forwarding or stalling necessary, just access memory as usual 
-		 deque<tuple<int,int,bool,bool>>::iterator recent_store_ptr = most_recent_memop(store_queue_map.at(addr),curr_memop, addr);  		 
-		 if(n->type==LD && most_recent_memop(store_queue_map.at(addr),curr_memop, addr)!=store_queue_map.at(addr).end()) { //meaning we found an older store 		   
-		   if(!memop_started(*recent_store_ptr)){ 
-		     c->next_active_list.push_back(n);
-		     c->next_start_set.insert(n);
-		     continue;
-		   }	       
-		   else{ //here, no need to stall AND we can receive load from prev store
-		     memory.at(n->id).pop();
-		     c->next_active_list.push_back(n);    
-		     cout << "Node [" << n->name << " @ context " << c->id << "]: Receiving Store-Load Forwarding for Address "<< addr << "\n";
-		     update_memop_queue(load_queue_map,addr,make_tuple(c->id,n->id,true,false)); //set entry in load queue to started
-		     c->remaining_cycles_map.at(n)=0; //single cycle access to store queue		   
-		     continue;
-		   }
-		 }
-	    	       
-		 if(n->type==ST && recent_store_ptr!=store_queue_map.at(addr).end()){ //there's actually an older stor
-		   update_memop_queue(store_queue_map,addr,make_tuple(c->id,n->id,true,false)); 
-		   if(!memop_completed(*recent_store_ptr)){
-		     //add to store queue, mark as started (so we can forward to loads), but can't push to DRAM yet
-		     c->next_active_list.push_back(n);
-		     c->next_start_set.insert(n);
-		     continue;
-		   } //stores must commit (into DRAMSim) in order 		 
-		 }
+               bool can_receive_forward=false;
+               bool must_stall=false;
+               tuple<int,int,bool,bool> curr_memop=make_tuple(c->id,n->id,false,false);
+              
+             
+               if (store_queue_map.find(addr)!=store_queue_map.end()) { //store queue for addr exists, otherwise no forwarding or stalling necessary, just access memory as usual 
+                  deque<tuple<int,int,bool,bool>>::iterator recent_store_ptr = most_recent_memop(store_queue_map.at(addr),curr_memop, addr);       
+                  if (n->type==LD && most_recent_memop(store_queue_map.at(addr),curr_memop, addr)!=store_queue_map.at(addr).end()) { //meaning we found an older store        
+                     if (!memop_started(*recent_store_ptr)) { 
+                        c->next_active_list.push_back(n);
+                        c->next_start_set.insert(n);
+                        continue;
+                     }         
+                     else { //here, no need to stall AND we can receive load from prev store
+                        memory.at(n->id).pop();
+                        c->next_active_list.push_back(n);    
+                        cout << "Node [" << n->name << " @ context " << c->id << "]: Receiving Store-Load Forwarding for Address "<< addr << "\n";
+                        update_memop_queue(load_queue_map,addr,make_tuple(c->id,n->id,true,false)); //set entry in load queue to started
+                        c->remaining_cycles_map.at(n)=0; //single cycle access to store queue       
+                        continue;
+                     }
+                  }
+                   
+                  if (n->type==ST && recent_store_ptr!=store_queue_map.at(addr).end()) { //there's actually an older stor
+                     update_memop_queue(store_queue_map,addr,make_tuple(c->id,n->id,true,false)); 
+                     if (!memop_completed(*recent_store_ptr)) {
+                        //add to store queue, mark as started (so we can forward to loads), but can't push to DRAM yet
+                        c->next_active_list.push_back(n);
+                        c->next_start_set.insert(n);
+                        continue;
+                     } //stores must commit (into DRAMSim) in order      
+                  }
                }
-	       else{
-		 cout << "Node [" << n->type << " @ context " << c->id << "]: NOT Receiving Store-Load Forwarding for Address "<< addr << "\n";
-	       }
-	       
+               else {
+                  cout << "Node [" << n->type << " @ context " << c->id << "]: NOT Receiving Store-Load Forwarding for Address "<< addr << "\n";
+               }
+          
                memory.at(n->id).pop(); // ...and take it out of the queue
                cout << "Node [" << n->name << " @ context " << c->id << "]: Inserts Memory Transaction for Address "<< addr << "\n";
                assert(mem->willAcceptTransaction(addr));
                if (n->type == LD){ 
-                  mem->addTransaction(false, addr);		  
-	          update_memop_queue(load_queue_map,addr,make_tuple(c->id,n->id,true,false)); //set entry in load queue to started
-	       }
-               else if (n->type == ST){
-		 mem->addTransaction(true, addr);
-		 update_memop_queue(store_queue_map,addr,make_tuple(c->id,n->id,true,false)); //set entry in store queue to started
-	       }
-               if(cb.outstanding_accesses_map.find(addr) == cb.outstanding_accesses_map.end())
-		 cb.outstanding_accesses_map.insert(make_pair(addr, queue<std::pair<Node*, Context*>>()));
+                  mem->addTransaction(false, addr);        
+                  update_memop_queue(load_queue_map,addr,make_tuple(c->id,n->id,true,false)); //set entry in load queue to started
+               }
+               else if (n->type == ST) {
+                  mem->addTransaction(true, addr);
+                  update_memop_queue(store_queue_map,addr,make_tuple(c->id,n->id,true,false)); //set entry in store queue to started
+               }
+               if (cb.outstanding_accesses_map.find(addr) == cb.outstanding_accesses_map.end())
+                  cb.outstanding_accesses_map.insert(make_pair(addr, queue<std::pair<Node*, Context*>>()));
                cb.outstanding_accesses_map.at(addr).push(make_pair(n, c));
             }
          }
@@ -329,22 +329,22 @@ public:
          // decrease the remaining # of cycles for current node <n>
          int remaining_cycles = c->remaining_cycles_map.at(n);
          if (remaining_cycles > 0)
-           remaining_cycles--;
+            remaining_cycles--;
          if ( remaining_cycles > 0 || remaining_cycles == -1 ) {  // Node <n> will continue execution in next cycle 
             // A remaining_cycles == -1 represents a outstanding memory request being processed currently by DRAMsim
             c->remaining_cycles_map.at(n) = remaining_cycles;
             c->next_active_list.push_back(n);  
          }
          else if (remaining_cycles == 0) { // Execution Finished for node <n>
-	    cout << "Node [" << n->name << " @ context " << c->id << "]: Finished Execution \n";
-	   
-	    // If node is a TERMINATOR create new context with next bbid in <cf> (a bbid list)
+            cout << "Node [" << n->name << " @ context " << c->id << "]: Finished Execution \n";
+      
+            // If node is a TERMINATOR create new context with next bbid in <cf> (a bbid list)
             if (n->type == TERMINATOR) {
-      	      if(cf.size() > c->id+1) {  // if there are more pending contexts in the Graph
+               if(cf.size() > c->id+1) {  // if there are more pending contexts in the Graph
                  assert(context_to_create == -1);
                  context_to_create = cf.at(c->id+1);  
                }
-      	   }
+            }
             c->remaining_cycles_map.erase(n);  
             if (n->type != NAI)
                c->processed++;
