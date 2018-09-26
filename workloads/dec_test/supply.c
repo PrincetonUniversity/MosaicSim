6e5ba125ded2b5c61ca1a6bf5bf4507edfc7f6fd
Tae Jun <ham.taejun@gmail.com>
2018-09-26 10:20:00 -0400

    dec

---
+++ workloads/dec_test/supply.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "decouple.h"
+
+void desc_kernel_add(int *a, int *b, int *c) {
+
+  //load A and B  
+  desc_produce_i32(a); //int reg_a = *a;
+  desc_produce_i32(b); //int reg_b = *b;
+
+  //Do the addition
+  //int val = reg_a + reg_b;
+
+  //Store the result
+  desc_store_addr_i32(c); //*c = val;
+}
+
+
+int main() {
+  desc_init(SUPPLY);
+  int a = 5, b = 6, c = 0;
+  
+  desc_kernel_add(&a, &b, &c);
+  
+  printf("got the value %d\n", c);
+  
+  return 0;
+}
