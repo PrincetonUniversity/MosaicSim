80f4433c8aa02e5b14d06e76e844666571119e09
luwamatt <luwa.matthews@gmail.com>
2019-05-12 14:13:27 -0400

    full simpleDRAM with BW throttling

---
+++ sim/memsys/SimpleDRAM.cc
@@ -0,0 +1,41 @@
+#include "SimpleDRAM.h"
+#include "DRAM.h"
+
+bool SimpleDRAM::process() {
+  
+  //handle all ready dram requests for this cycle
+  //cout << "max req epoch " << max_req_per_epoch << endl;
+ 
+  while(pq.size()>0) {
+    MemOperator memop=pq.top();
+    if(memop.final_cycle > cycles || request_count>=max_req_per_epoch) {
+      break;
+    }
+    if(memop.isStore) {
+      memInterface->write_complete(memop.trans_id,memop.addr,cycles);
+    }
+    else {
+      memInterface->read_complete(memop.trans_id,memop.addr,cycles);
+    }
+    request_count++;  
+    pq.pop();
+  }
+  
+  cycles++;
+  //reset request count every epoch
+  if(cycles % epoch_length==0) {
+    request_count=0;
+  }
+  return true;
+}
+
+bool SimpleDRAM::willAcceptTransaction(uint64_t addr) {
+  return addr==addr;
+}
+
+void SimpleDRAM::addTransaction(bool isStore, uint64_t addr) {  
+
+  MemOperator memop;
+  memop.addr=addr; memop.isStore=isStore; memop.trans_id=trans_id++; memop.final_cycle=cycles+latency;
+  pq.push(memop); //push to priority queue     
+}
