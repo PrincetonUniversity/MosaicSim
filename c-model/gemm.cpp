c042e2fce167979cf65bd6becab649639640b08e
Davide Giri <davide_giri@cs.columbia.edu>
2019-05-22 11:22:00 -0400

    reorganize slightly the c-model for gemm

--- c-model/gemm.cpp
+++ c-model/gemm.cpp
@@ -256,3 +256,30 @@ acc_perf_t dec_gemm_invoke(config_gemm_t config)
 
     return perf;
 }
+
+// simulator API to invoke GeMM accelerator
+acc_perf_t sim_gemm(config_gemm_t config)
+{
+    int n_invocations = (int) config.batch_size;
+
+    acc_perf_t perf = dec_gemm_invoke(config);
+
+    perf.cycles = perf.cycles * n_invocations;
+    perf.bytes = perf.bytes * n_invocations;
+
+    // int n_invocations = (int) config.batch_size / N_ACC_GEMM;
+
+    // // Call the accelerator only once, then project performance to
+    // // n_invocations of N_ACC_GEMM accelerator in parallel. The last
+    // // invocation might invoke less than N_ACC_GEMM accelerators.
+    // acc_perf_t perf = dec_gemm_invoke(config);
+
+    // perf.cycles = perf.cycles * n_invocations;
+    // perf.power = perf.power *
+    // 	((float) config.batch / (float) n_invocations);
+    // perf.bandwidth = perf.bandwidth *
+    // 	((float) config.batch / (float) n_invocations);
+    // perf.utilization = (float) config.batch / (float) n_invocations;
+
+    return perf;
+}
--- simulator-api/sim-gemm.c
+++ simulator-api/sim-gemm.c
@@ -4,26 +4,6 @@
 
 #include "sim_accelerators.h"
 
-// simulator API to invoke GeMM accelerator
-acc_perf_t sim_gemm(config_gemm_t config)
-{
-    int n_invocations = (int) config.batch_size / N_ACC_GEMM;
-
-    // Call the accelerator only once, then project performance to
-    // n_invocations of N_ACC_GEMM accelerator in parallel. The last
-    // invocation might invoke less than N_ACC_GEMM accelerators.
-    acc_perf_t perf = dec_gemm_invoke(config);
-
-    perf.cycles = perf.cycles * n_invocations;
-    perf.power = perf.power *
-	((float) config.batch / (float) n_invocations);
-    perf.bandwidth = perf.bandwidth *
-	((float) config.batch / (float) n_invocations);
-    perf.utilization = (float) config.batch / (float) n_invocations;
-
-    return perf;
-}
-
 int main(void)
 {
     // prepare accelerator configuration.
