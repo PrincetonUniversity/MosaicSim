103e746921ad94847dee7bac2cc516f65470d6c0
Tyler Sorensen <ts20@cafe.CS.Princeton.EDU>
2019-05-23 20:13:14 -0400

    oops, left a print statement in on accident

--- pass/profiler/RecordDynamicInfo.cpp
+++ pass/profiler/RecordDynamicInfo.cpp
@@ -240,8 +240,6 @@ namespace {
       PointerType* vPtrType = cast<PointerType>(v->getType());
       uint64_t storeSize = dl->getTypeStoreSize(vPtrType->getPointerElementType());
       ConstantInt *csize = llvm::ConstantInt::get(ctx, llvm::APInt(32, storeSize, true));
-      errs() << "KTYPE " << KERNEL_TYPE << " \n";
-      errs() << "RDIR  " << RUN_DIR << " \n";
       Value *ktype= Builder.CreateGlobalStringPtr(KERNEL_TYPE);
       Value *rdir = Builder.CreateGlobalStringPtr(RUN_DIR);
       Value* args[] = {name, ktype, rdir, ctype, castI, csize};
